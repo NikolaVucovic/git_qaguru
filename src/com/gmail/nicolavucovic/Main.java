52556d1887f70474691755672f1534813a0a1b9b
nico <nicolavucovic@gmail.com>
2022-04-01 13:50:32 +0200

    new commit

--- src/com/gmail/nicolavucovic/Main.java
+++ src/com/gmail/nicolavucovic/Main.java
@@ -3,6 +3,6 @@ package com.gmail.nicolavucovic;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("изучаем гит вместе с QA.GURU");
+        System.out.println("изучаем гит вместе с QA.GURU!");
     }
 }
