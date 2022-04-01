d9a9a6595a5fe09109ad85c84dd1d9de0fe2d95e
nico <nicolavucovic@gmail.com>
2022-04-01 14:49:01 +0200

    added new line

--- src/com/gmail/nicolavucovic/Main.java
+++ src/com/gmail/nicolavucovic/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Изучаем гит вместе с QA.GURU!");
+        System.out.println("Ох, как это не прсто");
     }
 }
